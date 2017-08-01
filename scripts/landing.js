abe2c8d0c51ee16026681f80bbede05f796f14e7
meghan222 <meghan222@github.com>
2017-08-01 12:30:11 -0500

    checkpoint-9-assigment-corrected

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,24 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-  points.style.opacity = 1;
-  points.style.transform = "scaleX(1) translateY(0)";
-  points.style.msTransform = "scaleX(1) translateY(0)";
-  points.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
  var animatePoints = function(points) {
-    forEach(points,revealPoint);
+    var revealPoint = function(index) {
+      points[index].style.opacity = 1;
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+
+    for (var i = 0; i < points.length; i++ ) {
+      revealPoint(i);
+    }
   };
     window.onload = function() {
       if (window.innerHeight > 950) {
          animatePoints(pointsArray);
-    }
+     }
 
       window.addEventListener("scroll", function(event) {
-        var sellingPoints = document.getElementsByClassName('selling-points')[0];
-        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-        if (document.documentElement.scrollTop || document.body.scrollTop>= scrollDistance) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
           animatePoints(pointsArray);
         }
       });
