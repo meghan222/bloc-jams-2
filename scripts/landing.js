42cbf32ab5f8abebe02c2424602b6ab8d2e49d4a
meghan222 <meghan222@github.com>
2017-08-01 12:26:40 -0500

    commit 1

--- scripts/landing.js
+++ scripts/landing.js
@@ -14,10 +14,12 @@ var revealPoint = function(point) {
          animatePoints(pointsArray);
     }
 
-      window.addEventListener('scroll', function(event) {
+      window.addEventListener("scroll", function(event) {
+        var sellingPoints = document.getElementsByClassName('selling-points')[0];
+        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
         if (document.documentElement.scrollTop || document.body.scrollTop>= scrollDistance) {
           animatePoints(pointsArray);
         }
       });
     }
-  
